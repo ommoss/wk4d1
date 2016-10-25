e537158340ffa9b8df3e882ad0b4b74ca7fc72d6
Owen Moss <ommoss63@gmail.com>
2016-10-25 19:49:06 +0000

    init

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE famous_people (
+  id BIGSERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  birthdate DATE
+);
---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Abraham', 'Lincoln', '1809-02-12');
+INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Mahatma', 'Gandhi', '1869-10-02');
+INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Paul', 'Rudd', '1969-04-06');
